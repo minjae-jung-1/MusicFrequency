e40e5febf0001b23d1a900773e46668cabd4cd0c
Minjae <minjae.jung0@gmail.com>
2023-02-22 21:07:07 -0500

    update

--- music-frequency/src/App.jsx
+++ music-frequency/src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const [music, setMusic] = useState(null);
+
+
+
+  const audioTune = new Audio('<YOUR_AUDIO_FILE_PATH.mp3>');
+
+  console.log(audioTune)
+ 
+  // variable to play audio in loop
+  const [playInLoop, setPlayInLoop] = useState(false);
+ 
+  // load audio file on component load
+  useEffect(() => {
+    audioTune.load();
+  }, [])
+ 
+  // set the loop of audio tune
+  useEffect(() => {
+    audioTune.loop = playInLoop;
+  }, [playInLoop])
+ 
+  // play audio sound
+  const playSound = () => {
+    audioTune.play();
+  }
+ 
+  // pause audio sound
+  const pauseSound = () => {
+    audioTune.pause();
+  }
+ 
+  // stop audio sound
+  const stopSound = () => {
+    audioTune.pause();
+    audioTune.currentTime = 0;
+  }
+//   const selectMusic = (e, file) => {
+//     if(file.size < 10485760 && file.type.includes('audio/')){
+//         const reader = new FileReader()
+
+//         reader.readAsDataURL(file)
+
+//         reader.onload = () => {
+//             console.log(reader)
+//         }
+//     }
+// }
 
   return (
     <div className="App">
@@ -20,8 +66,14 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <img src={music}></img>
-      <input type="file" onChange={(e)=>{console.log(e.files)}}/>
+      {/* <img src={music}></img> */}
+      <div>
+        <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound}></input>
+        <input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound}></input>
+        <input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound}></input>
+        <label><input type="checkbox" checked={playInLoop} onChange={e => setPlayInLoop(e.target.checked)} /> Play in Loop</label>
+      </div>
+      <input type="file" onChange={(e)=>{console.log(e)}}/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
